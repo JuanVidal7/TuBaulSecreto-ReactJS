64d64a085604230d2a21888f218ba3dc0a495ca2
Juan Vidal <juancarlosvidalp@hotmail.com>
2021-05-05 17:20:02 -0500

    Mejoras Desafío 11 Cart View

--- src/components/CardWidget.js
+++ src/components/CardWidget.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import '../css/style.css';
 import facebook from './facebook.png';
 import instagram from './instagram.png';
-import carrito from './carrito.png';
-import {useHistory} from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import { CartContext } from './CartContext';
+import logocarrito from './carrito.png';
 
 
 export default function CardWidget() {
+    const { carrito } = useContext(CartContext)
+    const itemsQtyCart = carrito.length;
+
     let history = useHistory();
     return (
         <header className="main-header">
-            <a href="http://facebook.com" target="_blank" rel="noopener noreferrer" ><img className="main-header__social" src={facebook}   
+            <a href="http://facebook.com" target="_blank" rel="noopener noreferrer" ><img className="main-header__social" src={facebook}
                 alt="facebook" /></a>
             <a href="http://instagram.com" target="_blank" rel="noopener noreferrer"><img className="main-header__social" src={instagram}
                 alt="instagram" /></a>
             <a href="#/" data-toggle="modal" data-target="#staticBackdrop" onClick={() => history.push("/cart")}><img
-                className="main-header__social" src={carrito} alt="Carrito" /></a>
+                className="main-header__social" src={logocarrito} alt="Carrito" style={{marginRight: '-10px'}} />{carrito.length !== 0 && <span className="red">{itemsQtyCart}</span>}</a>
         </header>
     )
 }
